382a3d237b22440355da6e987f5cbda3c230b5c8
kernamx <powen.chang1@gmail.com>
2017-10-21 17:48:24 -0700

    added some ipythontools

--- test_categorize_new_member.py
+++ test_categorize_new_member.py
@@ -2,18 +2,20 @@
 import pytest
 
 
-test.assert_equals(
-    openOrSenior([[45, 12], [55, 21], [19, -2], [104, 20]]),
-    ['Open', 'Senior', 'Open', 'Senior']
-)
-
-test.assert_equals(
-    openOrSenior([[16, 23], [73, 1], [56, 20], [1, -1]]),
-    ['Open', 'Open', 'Senior', 'Open']
-)
+# open_or_senior1 = (
+#     [[45, 12], [55, 21], [19, -2], [104, 20]],
+#     ['Open', 'Senior', 'Open', 'Senior']
+# )
 
 
-def test_open_or_senior([age, handicap], [result]):
+# open_or_senior2 = (
+#     [[16, 23], [73, 1], [56, 20], [1, -1]],
+#     ['Open', 'Open', 'Senior', 'Open']
+# )
+
+
+# @pytest.mark.parametrize([age, handicap], open_or_senior1)
+def test_open_or_senior_45_12():
     """."""
     from categorize_new_member import open_or_senior
-    assert open_or_senior(data) == result
+    assert open_or_senior(45, 12) == 'Open'
