52d1d0f296fc5bda256ecc9fedd480a9bab58bd5
kernamx <powen.chang1@gmail.com>
2017-10-24 23:46:00 -0700

    fixed the module name and the function

--- sum_of_multiples.py
+++
@@ -1,9 +0,0 @@
-"""Sum of multiples."""
-
-
-def sum_mul(n, m):
-    """Thi will return sum of multiples."""
-    if m > 0 and n > 0:
-        return sum(range(n, m, n))
-    else:
-        return 'INVALID'
